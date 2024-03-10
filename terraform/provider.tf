ea882cffec14bf95f86b6d112b186d65c50368dc
gshashi1408 <154942929+gshashi1408@users.noreply.github.com>
2024-03-10 17:33:16 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,11 +1,12 @@
-provider "aws" {
-    region = "ap-south-1"
-}
- 
 terraform {
-    backend "s3" {
-      region = "ap-south-1"
-      bucket = "tarundanda147"
-      key = "state.tfstate"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
 }
